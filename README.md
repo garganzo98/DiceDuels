4a408cbdfb1da439a47379f363405a107261337b
garganzo98 <egarcia@sjnma.org>
2017-02-12 13:40:40 -0600

    rules

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DiceDuels
+player 1 vs player 2
+20 sided die 
+each player will roll both die each turn
+only one turn 
---
+++ diceduels.py
@@ -0,0 +1,14 @@
+from random import randint
+
+print("roll the dice homeboy, if you win, you a G")
+
+playerone = raw_input("what is your name player one")
+playertwo = raw_input("what is your name player two")
+
+roll = raw_input("press enter to roll the dice")
+rollone = randint(1,6)
+rolltwo = randint(1,6)
+rollthree = randint(1,6)
+print(rollone)
+print(rolltwo)
+print(rollthree)
